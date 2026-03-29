f8ee312ad24b280750e25f331ee806c87e32ce21
AnlinWilliam <anlinwilliam79@gmail.com>
2026-03-29 21:48:29 +0530

    added Procfile and requirements.txt

--- app.py
+++ app.py
@@ -1735,5 +1735,8 @@ def get_health_services():
 
 
 # -------- RUN SERVER --------
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
